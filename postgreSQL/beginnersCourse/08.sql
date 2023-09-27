677371dced6c083eb3a728003c4d266c1b42f93a
David Castellanos <davcastellanoslarios@gmail.com>
2023-09-27 17:52:33 -0600

    comparaciones

---
+++ postgreSQL/beginnersCourse/08.sql
@@ -0,0 +1,13 @@
+-- NUMERIC conditional true and false  
+SELECT 1 = 1;
+
+-- grater than 
+SELECT 1 <= 2;
+
+-- less than 
+SELECT 1 >= 2;
+
+-- not equal  <>
+SELECT 1 <> 2;
+SELECT 'AMIGOSCODE' <> 'amigoscode';
+
