c0be2b4c8abe2ff1d6516e82f3d452da2750ef22
David Castellanos <davcastellanoslarios@gmail.com>
2023-09-27 16:03:05 -0600

    how to create table with postgres

---
+++ postgreSQL/beginnersCourse/03.sql
@@ -0,0 +1,20 @@
+CREATE TABLE users (
+    id int,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    gender VARCHAR(6),
+    date_of_birth TIMESTAMP,
+);
+
+CREATE TABLE person (
+    id INT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    gender VARCHAR(7),
+    date_of_birth DATE 
+);
+
+-- review tables
+-- \d
+-- selecte the tables
+-- \d nametable
