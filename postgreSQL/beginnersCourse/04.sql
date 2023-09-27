63500e123101a8ab890370ef1f6a33baed2cf66c
David Castellanos <davcastellanoslarios@gmail.com>
2023-09-27 16:09:17 -0600

    done

---
+++ postgreSQL/beginnersCourse/04.sql
@@ -0,0 +1,13 @@
+-- drop a table 
+
+DROP TABLE  person;
+
+-- improve table 
+CREATE TABLE person (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    gender VARCHAR(7) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    emial VARCHAR(150)
+);
