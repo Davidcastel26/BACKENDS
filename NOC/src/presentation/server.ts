5e5af03072e82207ad8c6596f7765b055cb5345e
desa04 <david.castellanos@popoyan.com.gt>
2023-12-14 17:10:58 -0600

    Update server.ts

--- NOC/src/presentation/server.ts
+++ NOC/src/presentation/server.ts
@@ -1,23 +1,35 @@
 // import { envs } from "../config/plugins/envs.plugins";
-import { CheckService } from "../domain/useCases/checks/checkService";
+// import { CheckService } from "../domain/useCases/checks/checkService";
+import { SendEmailLogs } from "../domain/useCases/logs/email/sendLogs";
 import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImp } from "../infrastructure/repository/log.respository.imp";
-import { CronService } from "./cron/cronTask";
+// import { CronService } from "./cron/cronTask";
 import { EmailService } from "./email/email.service";
 
 const fileSystemLogRepository = new LogRepositoryImp(
     new FileSystemDataSource()
 );
 
+const emailService = new EmailService();
+
 export class Server {
     
     public static start(){
+
+        
 
         console.log('Server Started...');
         // console.table({key: envs.MAILER_SECRET_KEY, mail: envs.MAILER_EMAIL})
         
         // CLASS 4 SEND Email 
-        const emailService = new EmailService(fileSystemLogRepository);
+        // new SendEmailLogs(
+        //     emailService,
+        //     fileSystemLogRepository
+        // ).execute([
+        //     'david.castellanos@popoyan.com.gt'
+        // ])
+        // const emailService = new EmailService(fileSystemLogRepository);
+            
         // send mails---------- 
         // emailService.sendEmailWithFilesSystemLog([
         //     'david.castellanos@popoyan.com.gt'
