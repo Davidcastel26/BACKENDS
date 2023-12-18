a38ce6bb30edccaffebe379777398b0ee5e56a48
Davidcastel26 <davcastellanoslarios@gmail.com>
2023-12-18 09:17:09 -0600

    Update log.entity.test.ts

--- NOC/src/domain/entities/log.entity.test.ts
+++ NOC/src/domain/entities/log.entity.test.ts
@@ -4,13 +4,16 @@ import { LogEntity } from "./log.entity"
 
 describe('Log ENTITY into log.entity.test.ts', () => { 
     
+
+    const dataObj = {
+        message: 'Hola mundo',
+        level: LogSeverityLevel.high,
+        origin: '/entities/log.entity.ts'
+    }
+
     test('should create a log entity instance', () => { 
 
-        const dataObj = {
-            message: 'Hola mundo',
-            level: LogSeverityLevel.high,
-            origin: '/entities/log.entity.ts'
-        }
+        
         
         const log = new LogEntity(dataObj);
 
@@ -31,6 +34,18 @@ describe('Log ENTITY into log.entity.test.ts', () => {
         expect( log.message ).toBe( "Service http://google.com working" );
         expect( log.level ).toBe( LogSeverityLevel.low );
         expect( log.origin ).toBe("check-service.ts"  );
+        expect( log.createdAt).toBeInstanceOf(Date);
+
+      })
+
+      test('should create a LogEntity instance from Object', () => {
+
+        const log = LogEntity.fromObject(dataObj)
+
+        expect( log ).toBeInstanceOf( LogEntity  );
+        expect( log.message ).toBe(dataObj.message);
+        expect( log.level ).toBe(  dataObj.level  );
+        expect( log.origin ).toBe( dataObj.origin );
         expect( log.createdAt).toBeInstanceOf(Date);
 
       })
