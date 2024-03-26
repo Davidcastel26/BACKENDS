6d304446a95b190293a73024f18d2a9764cf6534
Davidcastel26 <davcastellanoslarios@gmail.com>
2024-03-26 16:52:23 -0600

    breadthFirstPrint

---
+++ repasos/depthFirstPrintRecursive.js
@@ -0,0 +1,20 @@
+
+depthFirstPrintRecursive = ( graph, source ) => {
+
+    console.log(source)
+    for( let neighbor of graph[source]){
+        depthFirstPrintRecursive( graph, neighbor)
+    }
+
+}
+
+const graphy = {
+    a:['b','c'],
+    b:['d'],
+    c:['e'],
+    d:['f'],
+    e:[''],
+    f:[''],
+}
+
+depthFirstPrintRecursive( graphy, 'a')
