5d72d9ee1407d711046faeac970423301641452c
Davidcastel26 <davcastellanoslarios@gmail.com>
2024-04-03 10:30:34 -0600

    development: depthfirst has path with info and time complexity

--- repasos/depthFirstHasPath.js
+++ repasos/depthFirstHasPath.js
@@ -1,17 +1,46 @@
-const hasPath = ( graph, src, dst) => {
+// this is the complex
+// n = # nodes
+// e = # edges
+// time: 0(e)
+// space: 0(n)
+
+//n as a single variable
+// n = # nodes
+// n^2 = # edges
+// time = 0(n^2)
+// space = 0(n)
+
+const hasPath = ( 
+  graph, 
+  src, // location
+  dst,  // destination
+) => {
+   //its the src what im looking for return true 
+    if( src == dst ) return true
+    // console.log(src)
+  // its not the same ->
+  // now lets loop for every single array form the obj of the graph
   
-    if( src === dst ) true
-    
-    for( let neigbor of graph[src]){
-      if( hasPath( graph, neigbor, dst) === true) {
-        return true
+    for( let neighbor of graph[src]){
+      // console.log(neighbor)
+      //create the recurstion but now the src will be the neighbor we found
+      // hasPath( graph, neighbor, dst)
+      // we dont set return false since we need to continues till the end of the edges
+      // console.log( neighbor)
+      // console.log( graph[neighbor] )
+    if( hasPath( graph, neighbor, dst) === true ){
+      // console.log( neighbor)
+      // console.log( graph[neighbor] )
+       return true 
       }
     }
+  
+  // until we loop everthing return false
     return false
   }
   
-  
-  const graph1 = {
+
+  const graph = {
     f:['g','i'],
     g:['h'],
     h:[],
@@ -21,4 +50,4 @@ const hasPath = ( graph, src, dst) => {
   }
   
   
-  hasPath( graph1, 'f', 'k')
+  hasPath( graph, 'f', 'k')
