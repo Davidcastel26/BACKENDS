10173b0195197f12bc23e34021068ce6f51e71a5
Davidcastel26 <davcastellanoslarios@gmail.com>
2024-04-09 13:07:23 -0600

    main: create logoutHandler to logout

--- jwt_phase/src/controllers/auth.controller.ts
+++ jwt_phase/src/controllers/auth.controller.ts
@@ -167,3 +167,17 @@ export const refreshAccessTokenHandler = async (
   }
 };
 
+export const logoutHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+   const user = res.locals.user;
+    await redisClient.del(user._id);
+    logout(res);
+    return res.status(200).json({ status: 'success' });
+  } catch (err: any) {
+    next(err);
+  }
+};
